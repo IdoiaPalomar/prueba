a331d72ea657484685855cc2a6a1ee7abaa53851
IdoiaPalomar <idoya.palomar@gmail.com>
2018-02-26 14:00:54 +0100

    añadido clase prueba

---
+++ prueba.java
@@ -0,0 +1,28 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class prueba
+{
+     
+    private int x;
+
+    /**
+     * Constructor  
+     */
+    public prueba()
+    {
+         
+    }
+
+    /**
+     *  
+     */
+    public void metodo()
+    {
+         
+    }
+}
